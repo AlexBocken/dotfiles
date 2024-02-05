3e1222e211dbf987908f27703a18a73b8ce23cd7
Alexander Bocken <alexander@bocken.org>
2024-02-05 09:25:33 +0100

    qutebrowser: remove more yt ads

--- .local/share/qutebrowser/greasemonkey/remove_more_yt_ads.js
+++ .local/share/qutebrowser/greasemonkey/remove_more_yt_ads.js
@@ -7,7 +7,7 @@
 // @exclude      *://*.youtube.com/subscribe_embed?*
 // ==/UserScript==
 setInterval(() => {
-    document.querySelectorAll('.ytd-page-top-ad-layout-renderer,.ytd-video-display-full-buttoned-renderer, ytd-display-ad-renderer').forEach(
+    document.querySelectorAll('.ytd-page-top-ad-layout-renderer,.ytd-video-display-full-buttoned-renderer, ytd-display-ad-renderer,.ytd-banner-promo-renderer-background,.ytd-player-legacy-desktop-watch-ads-renderer').forEach(
 	el => {
 			el.remove();
 	})
