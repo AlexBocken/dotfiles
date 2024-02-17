ba4f3a72a0d2dccc782524cf527b780b237a26f2
Alexander Bocken <alexander@bocken.org>
2024-02-17 11:00:39 +0100

    update for new gridview ads

--- .local/share/qutebrowser/greasemonkey/remove_more_yt_ads.js
+++ .local/share/qutebrowser/greasemonkey/remove_more_yt_ads.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Auto Remove Ads
-// @version      1.0.0
+// @version      1.0.1
 // @description  Autoremove static ads
 // @author       Alexander Bocken
 // @match        *://*.youtube.com/*
 // @exclude      *://*.youtube.com/subscribe_embed?*
 // ==/UserScript==
 setInterval(() => {
-    document.querySelectorAll('.ytd-page-top-ad-layout-renderer,.ytd-video-display-full-buttoned-renderer, ytd-display-ad-renderer,.ytd-banner-promo-renderer-background,.ytd-player-legacy-desktop-watch-ads-renderer').forEach(
+    document.querySelectorAll('.ytd-page-top-ad-layout-renderer,.ytd-display-ad-renderer,.ytd-video-display-full-buttoned-renderer, ytd-display-ad-renderer,.ytd-banner-promo-renderer-background,.ytd-player-legacy-desktop-watch-ads-renderer').forEach(
 	el => {
 			el.remove();
 	})
